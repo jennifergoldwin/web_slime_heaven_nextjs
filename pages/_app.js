42098fe7dd40c0e6be81e9f496437d88256177ea
Jennifer Goldwin <jennifer.goldwin@binus.ac.id>
2022-09-18 12:22:20 +0700

    apply google analytics and microsoft clarity

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=G-TSNFQEYYCW`}
       />
 
       <Script strategy="lazyOnload">
@@ -13,11 +13,19 @@ function MyApp({ Component, pageProps }) {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        gtag('config', 'G-TSNFQEYYCW', {
         page_path: window.location.pathname,
         });
     `}
       </Script>
+      <Script type="text/javascript">
+        {`(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "doz2v84xaf");`}
+      </Script>
+
       <Component {...pageProps} />
     </>
   );
